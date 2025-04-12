12eddfcb672914ca84d010c9227a3a64452f5522
Sybren Roede <s.roede@zwijsen.eu>
2025-04-12 12:08:53 +0000

    wip

--- python/envtamer/envtamer/cli.py
+++ python/envtamer/envtamer/cli.py
@@ -10,9 +10,16 @@ ascii_art = r"""
 
 print(ascii_art)
 
-@click.Group
-def cli():
-    print('hello')
+@click.command()
+@click.option('--count', default=1, help='Number of greetings.')
+@click.option('--name', prompt='Your name',
+              help='The person to greet.')
+
+# @click.Group
+def cli(count, name):
+    """Simple program that greets NAME for a total of COUNT times."""
+    for x in range(count):
+        click.echo(f"Hello {name}!")
 
 if __name__ == '__main__':
     cli()
